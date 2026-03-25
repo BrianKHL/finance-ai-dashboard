a31283e40f8fe8156e08e0914143ec7b3a8c813c
Brian Lee <129114119+BrianKHL@users.noreply.github.com>
2026-03-25 08:43:06 -0500

    Initial React setup with Vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/**
+ * Blueprint of Dashboard program
+ */
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Finance AI Dashboard
+      </h1>
+    </div>
+  );
+}
+
+export default App;
