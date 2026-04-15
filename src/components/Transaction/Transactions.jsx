422ccf1e5dae2643b80564e9b3c06841538180ad
Brian Lee <129114119+BrianKHL@users.noreply.github.com>
2026-04-15 08:44:09 -0500

    Switch Transactions.jsx to use state for API connect later

--- src/components/Transaction/Transactions.jsx
+++ src/components/Transaction/Transactions.jsx
@@ -1,43 +1,55 @@
+import { useState, useEffect } from "react";
 import "./Transactions.css";
 import TransactionItem from "../TransactionItem/TransactionItem";
 
 function Transactions() {
-    const transactions = [
-    {
-        category: "Dinning",
-        date: '2023-01-01',
-        name: 'Chick-fil-A',
-        amount: -10.25,
-    },
-    {
-        category: "Online",
-        date: '2023-01-02',
-        name: 'Amazon',
-        amount: -80.88,
-    },
-    {
-        category: "Shopping",
-        date: '2023-01-03',
-        name: 'Target (Return)',
-        amount: +30,
-    },
-];
+    const [transactions, setTransactions] = useState([]);
 
-return (
-    <section className="transactions">
-        <h2>Recent Transactions</h2>
-        <div className="transaction-list">
-            {transactions.map((item, index) => (
-                <TransactionItem
-                    key={index}
-                    category={item.category}
-                    date={item.date}
-                    name={item.name}
-                    amount={item.amount}
-                />
-            ))}
-        </div>
-    </section>
-);
+    //Temporary mock data
+    useEffect(() => {
+        const mockData = [
+            {
+                id: 1,
+                category: "Dinning",
+                date: '2023-01-01',
+                name: 'Chick-fil-A',
+                amount: -10.25,
+            },
+            {
+                id: 2,
+                category: "Online",
+                date: '2023-01-02',
+                name: 'Amazon',
+                amount: -80.88,
+            },
+            {
+                id: 3,
+                category: "Shopping",
+                date: '2023-01-03',
+                name: 'Target (Return)',
+                amount: +30,
+            },
+        ];
+
+        setTransactions(mockData);
+    }, []);
+
+
+    return (
+        <section className="transactions">
+            <h2>Recent Transactions</h2>
+            <div className="transaction-list">
+                {transactions.map((item) => (
+                    <TransactionItem
+                        key={item.id}
+                        category={item.category}
+                        date={item.date}
+                        name={item.name}
+                        amount={item.amount}
+                    />
+                ))}
+            </div>
+        </section>
+    );
 }
 export default Transactions;
