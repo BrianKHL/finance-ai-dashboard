e706ac7dacfcd3dbd382413b33c949ef8dcdea87
Brian Lee <129114119+BrianKHL@users.noreply.github.com>
2026-04-17 08:10:59 -0500

    Lift state from Transactions.jsx to Dashboard.jsx

--- src/components/Transaction/Transactions.jsx
+++ src/components/Transaction/Transactions.jsx
@@ -1,40 +1,7 @@
-import { useState, useEffect } from "react";
 import "./Transactions.css";
 import TransactionItem from "../TransactionItem/TransactionItem";
 
 function Transactions() {
-    const [transactions, setTransactions] = useState([]);
-
-    //Temporary mock data
-    useEffect(() => {
-        const mockData = [
-            {
-                id: 1,
-                category: "Dinning",
-                date: '2023-01-01',
-                name: 'Chick-fil-A',
-                amount: -10.25,
-            },
-            {
-                id: 2,
-                category: "Online",
-                date: '2023-01-02',
-                name: 'Amazon',
-                amount: -80.88,
-            },
-            {
-                id: 3,
-                category: "Shopping",
-                date: '2023-01-03',
-                name: 'Target (Return)',
-                amount: +30,
-            },
-        ];
-
-        setTransactions(mockData);
-    }, []);
-
-
     return (
         <section className="transactions">
             <h2>Recent Transactions</h2>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Header from "../components/Header/Header";
 import UserInfo from "../components/UserInfo/UserInfo";
 import SummarySection from "../components/SummarySection/SummarySection";
@@ -6,14 +7,47 @@ import Transactions from "../components/Transaction/Transactions";
 import BudgetStatus from "../components/BudgetStatus/BudgetStatus";
 
 function Dashboard() {
+  const [transactions, setTransactions] = useState([]);
+
+  //Temporary mock data
+  useEffect(() => {
+    const mockData = [
+      {
+        id: 1,
+        category: "Dinning",
+        date: '2023-01-01',
+        name: 'Chick-fil-A',
+        amount: -10.25,
+      },
+      {
+        id: 2,
+        category: "Online",
+        date: '2023-01-02',
+        name: 'Amazon',
+        amount: -80.88,
+      },
+      {
+        id: 3,
+        category: "Shopping",
+        date: '2023-01-03',
+        name: 'Target (Return)',
+        amount: +30,
+      },
+  ];
+
+    setTransactions(mockData);
+  }, []);
+
+
+
   return (
     <div className="container">
       <Header />
       <UserInfo />
-      <SummarySection />
+      <SummarySection transactions={transactions}/>
       <Diagram />
-      <Transactions />
-      <BudgetStatus />
+      <Transactions transactions={transactions}/>
+      <BudgetStatus transactions={transactions}/>
     </div>
   );
 }
